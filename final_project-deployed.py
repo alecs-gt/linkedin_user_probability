f699017e5af71636de234626b0b08abed552612a
Alec Safreno <alecsafreno@gmail.com>
2025-12-08 22:34:27 -0800

    Update columns

--- final_project-deployed.py
+++ final_project-deployed.py
@@ -110,6 +110,7 @@ radar_labels = [
     f"Parent: {"Yes" if parent_bool else "No"}",
     f"Married: {"Yes" if married_bool else "No"}",
     f"Gender: {"Female" if female_bool else "Male"}"
+    f"Age: {int(round(radar_values["Age"] * 100))} years"
 ]
 
 labels = radar_labels
